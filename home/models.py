b99ba04bb15ddf31e5ce73e12a4be4d3f8eb9831
Prafful Kumar <kumarprafful.54@gmail.com>
2017-09-02 17:40:22 +0530

    creating ui

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Intro(models.Model):
+	title = models.CharField(max_length=255, default="Intro")
+	content = models.TextField()
+
+	def __str__(self):
+		return self.title
+
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Intro
 # Create your views here.
 def index(request):
-	return render(request, 'home/index.html')
+	intro = Intro.objects.all()
+	return render(request, 'home/index.html', {'intro':intro})
